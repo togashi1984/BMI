cdca174efc65a04aeb57691db45d2dc713ad5877
togashi1984 <wqfqw3200@kjb.biglobe.ne.jp>
2020-02-12 17:03:21 +0900

    Initial commit

---
+++ venv/BMI app.py
@@ -0,0 +1,16 @@
+
+height_m=int(input('身長は(cm)？＞'))/100
+
+weight_kg=int(input('体重（kg）は？'))
+
+
+
+BMI=weight_kg/(height_m ** 2)
+
+print(f'あなたのBMIは{BMI:.2f}です')
+
+
+
+
+
+
---
+++ venv/bmi.py
@@ -0,0 +1,6 @@
+height_m=(178/100)
+weight_kg=75
+
+bmi=weight_kg/ (height_m**2)
+
+print(f'{bmi:.2f}')
